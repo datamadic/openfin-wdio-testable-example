a2be00a6d0ee98eb0e1e4992e282fb2021248d7d
Xavier ONeil <xavier@openfin.co>
2019-09-24 21:50:15 -0400

    one take jake

--- test.js
+++ test.js
@@ -50,6 +50,32 @@ describe('Hello OpenFin App testing with webdriver.io', function() {
         }
     }
 
+    const gpConfig = 'https://openfin.github.io/golden-prototype/public.json';
+    const hofConfig = 'https://demoappdirectory.openf.in/desktop/config/apps/OpenFin/HelloOpenFin/app.json';
+    
+
+    it('should be able to create a new window in proc and switch to it', async () => {
+        const w = await fin.Window.create({name:'foo', url: 'about:blank'});
+        await w.executeJavaScript('document.title="new title"')
+        switchWindowByTitle('new title')
+        browser.testableScreenshot('new-win-about-blank');
+        
+    })
+
+    it('should be able to create a new window out of proc and switch to it', async () => {
+        const w = await fin.Window.create({name:'foo', url: 'https://example.com'});
+        switchWindowByTitle('Example Domain')
+        browser.testableScreenshot('new-win-out-of-proc');
+        
+    })
+
+    it('should create a new app and switch to it', async () => {
+        const app = await fin.Application.startFromManifest(gpConfig);
+        switchWindowByTitle('OpenFin Template')
+        browser.testableScreenshot('of-template');
+    });
+    
+
     it('Switch to Hello OpenFin Main window', () => {
         switchWindowByTitle("Hello OpenFin");
         browser.testableScreenshot('Main');
