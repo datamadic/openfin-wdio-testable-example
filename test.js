7e868a95c8fb8251feb626a1152f44522c46a461
Xavier ONeil <xavier@openfin.co>
2019-09-24 22:41:21 -0400

    one take jake

--- test.js
+++ test.js
@@ -54,26 +54,50 @@ describe('Hello OpenFin App testing with webdriver.io', function() {
     const hofConfig = 'https://demoappdirectory.openf.in/desktop/config/apps/OpenFin/HelloOpenFin/app.json';
     
 
-    it('should be able to create a new window in proc and switch to it', async () => {
-        const w = await fin.Window.create({name:'foo', url: 'about:blank'});
-        await w.executeJavaScript('document.title="new title"')
+    // it('should be able to create a new window in proc and switch to it', async () => {
+    //     const w = await fin.Window.create({name:'foo', url: 'about:blank'});
+    //     await w.executeJavaScript('document.title="new title"')
+    //     switchWindowByTitle('new title')
+    //     browser.testableScreenshot('new-win-about-blank');
+        
+    // })
+
+    it('should be able to create a new window in proc and switch to it', () => {
+        browser.setTimeout({ script: 5000 })
+        const result = browser.executeAsync(function(done) {
+            // browser context - you may not access client or console
+            (async()=>{
+                const w = await fin.Window.create({name:'foo', url: 'about:blank'});
+                await w.executeJavaScript('document.title="new title"');
+                done()
+            })()
+        });
+
         switchWindowByTitle('new title')
         browser.testableScreenshot('new-win-about-blank');
-        
-    })
-
-    it('should be able to create a new window out of proc and switch to it', async () => {
-        const w = await fin.Window.create({name:'foo', url: 'https://example.com'});
-        switchWindowByTitle('Example Domain')
-        browser.testableScreenshot('new-win-out-of-proc');
-        
-    })
-
-    it('should create a new app and switch to it', async () => {
-        const app = await fin.Application.startFromManifest(gpConfig);
-        switchWindowByTitle('OpenFin Template')
-        browser.testableScreenshot('of-template');
+        // node.js context - client and console are available
+        // console.log(result) // outputs: 10
     });
+
+    // it('should be able to create a new window out of proc and switch to it', async () => {
+    //     const w = await fin.Window.create({name:'foo-out-proc', url: 'https://example.com'});
+    //     switchWindowByTitle('Example Domain')
+    //     browser.testableScreenshot('new-win-out-of-proc');
+        
+    // })
+
+    // it('should be able to use the fin api', async () => {
+    //     const w = await fin.Window.create({name:'foo-out-proc', url: 'https://example.com'});
+    //     switchWindowByTitle('Example Domain')
+    //     browser.testableScreenshot('new-win-out-of-proc');
+        
+    // })
+
+    // it('should create a new app and switch to it', async () => {
+    //     const app = await fin.Application.startFromManifest(gpConfig);
+    //     switchWindowByTitle('OpenFin Template')
+    //     browser.testableScreenshot('of-template');
+    // });
     
 
     it('Switch to Hello OpenFin Main window', () => {
